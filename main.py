675dc0eed2887a973945047186379b769c13a48a
FranKo19Rp1997 <franko19rp@gmail.com>
2025-10-05 02:08:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -544,4 +544,4 @@ if __name__ == '__main__':
     print("  • Sistema de recomendación: http://localhost:5000/recommendation-system")
     print("=" * 50)
 
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
