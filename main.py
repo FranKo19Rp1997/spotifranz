71cf958af40fd0d5e8af7bac3f32f4c4e609d89b
Sebastián Antonio Morales Gonzalez <sebastianmoraless257@gmail.com>
2025-10-06 20:34:45 -0400

    prueba

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    # Ejecutar la aplicación en el puerto proporcionado por Rende
+    # Ejecutar la aplicación en el puerto proporcionado por Render
     print(f"App corriendo en el puerto {port}")
